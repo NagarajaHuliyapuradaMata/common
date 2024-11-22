5202b855878ecb7bb3f8b2a2962a2abda9a470e2
Nagaraja HULIYAPURADA MATA <Nagaraja.Huliyapurada-Mata@outlook.com>
2024-11-22 09:20:46 +0530

    Update: 202411141541PM

--- main_LATEST/TypesStd.hpp
+++ main_LATEST/TypesStd.hpp
@@ -23,10 +23,15 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-typedef unsigned char boolean;
-typedef signed   char sint8;
-typedef unsigned char uint8;
-typedef unsigned long uint32;
+typedef unsigned char  boolean;
+typedef signed   char  sint8;
+typedef signed   short sint16;
+typedef signed   int   sint32;
+typedef signed   long  sint64;
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned int   uint32;
+typedef unsigned long  uint64;
 
 /******************************************************************************/
 /* CONSTS                                                                     */
